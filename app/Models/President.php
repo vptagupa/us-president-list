adee38a58d33596aaa143a37d83a558596cab5a1
Vic <victortagupa@gmail.com>
2023-03-20 02:29:48 +0000

    Add president model and migration

--- app/Models/President.php
+++ app/Models/President.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Casts\Address;
+
 class President extends Model
 {
     use HasFactory;
@@ -16,7 +18,8 @@ class President extends Model
         'age',
         'address',
         'year_started',
-        'year_ended'
+        'year_ended',
+        'photo'
     ];
 
     protected $appends = ['fullname'];
@@ -24,10 +27,11 @@ class President extends Model
     protected $casts = [
         'year_started'  => 'date:Y-m-d',
         'year_ended'    => 'date:Y-m-d',
+        'address'       =>  Address::class,
     ];
 
     public function getFullNameAttribute()
     {
-
+        return $this->first_name." ".$this->middle_name." ".$this->last_name;
     }
 }
--- database/migrations/2023_03_20_014709_create_presidents_table.php
+++ database/migrations/2023_03_20_014709_create_presidents_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('presidents', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name', 35);
+            $table->string('middle_name', 35);
+            $table->string('last_name', 35);
+            $table->unsignedInteger('age');
+            $table->date('year_started');
+            $table->date('year_ended');
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
