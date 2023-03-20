512ea557c21ab86bbbd651ab30e18a62d58fe985
Vic <victortagupa@gmail.com>
2023-03-20 02:22:28 +0000

    Add cast Address

--- app/Casts/Address.php
+++ app/Casts/Address.php
@@ -5,6 +5,9 @@ namespace App\Casts;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
 
+use App\ValueObjects\AddressValueObject;
+use InvalidArgumentException;
+
 class Address implements CastsAttributes
 {
     /**
@@ -14,7 +17,10 @@ class Address implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value;
+        return new AddressValueObject(
+            $value['address_1'],
+            $value['address_2'],
+        );
     }
 
     /**
@@ -24,6 +30,13 @@ class Address implements CastsAttributes
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value;
-    }
+        if (! $value instanceof AddressValueObject) {
+            return new InvalidArgumentException("The given value is not an address instance.");
+        }
+
+        return [
+            'address_1' => $value->getAddress1(),
+            'address_2' => $value->getAddress2(),
+        ];
+    } 
 }
